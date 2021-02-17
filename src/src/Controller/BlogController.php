6f7d5cdf8f3b066afd79cd66122e0adb7cba6c87
Märt Märkson <admin@kysi.ee>
2021-02-17 12:47:00 +0200

    just the first commit

--- src/src/Controller/BlogController.php
+++ src/src/Controller/BlogController.php
@@ -11,6 +11,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\CommentType;
 
 /**
  * @Route("/blog")
